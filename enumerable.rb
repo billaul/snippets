be3e60d94ae29cd6aaee67a89e3a60f202e35a78
billaul <billau_l@modulotech.fr>
2021-02-11 18:05:21 +0100

    Update enumerable.rb

--- enumerable.rb
+++ enumerable.rb
@@ -29,11 +29,11 @@ module Enumerable
         "\r",
         # Name and percent
         "#{named} #{('%0.2f' % percent).rjust(7, ' ')}%",
-        # current item / index
+        # Current item index / total count
         "#{(index+1).to_s.rjust(count.to_s.length, ' ')}/#{count}",
         # Time spend
         "#{format.call(elapsed_time)}",
-        # Remaining Time
+        # Estimated remaining Time
         "(eta: #{remaining_time.present? ? format.call(remaining_time) : '...'})"
       ].join(' '))
       # Finaly call the block on the current value
@@ -46,7 +46,7 @@ module Enumerable
     count = self.count
     start_time = Time.zone.now
     remaining_time = nil
-    format = -> (time) { (((time / 60).round > 0) ? "#{(time/60).round}m" : '') + "#{(time % 60).round(2)}s" }
+    format = -> (time) { ((time / 60).round > 0 ? "#{(time/60).round}m" : '') + "#{(time % 60).round(2)}s" }
     self.with_index do |value, index|
       elapsed_time = (Time.zone.now-start_time)
       percent = index == 0 ? 0 : (index.to_f/count*100)
